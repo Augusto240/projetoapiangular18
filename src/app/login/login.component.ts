8ad1218a7a5c7804befb99e5f35c40aed1ae1ec1
José Augusto de Oliveira Neto <20231148060001@ifrn.local>
2024-08-15 14:01:14 -0300

    editing constructor on component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginUser } from '../login-user';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +16,25 @@ export class LoginComponent {
     senha = ''
     mensagemErro = ''
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
   onLogin():void{
     this.mensagemErro = ''
-    if (this.usuario === 'admin' && this.senha === '123'){
-      localStorage.setItem('token', this.usuario);
-      this.router
-    }
+    const USER: LoginUser = { email: this.usuario, senha: this.senha }
+    this.loginService.login(USER).subscribe({
+      next: (retorno: any) =>{
+        console.log(retorno)
+      },
+      error: (error) =>{
+        console.log(error)
+      }
+    })
+    
+    //if (this.usuario === 'admin' && this.senha === '123'){
+      //localStorage.setItem('token', this.usuario);
+      //this.router.navigate(['/'])
+    //} else{
+      //this.mensagemErro = 'Usuário e/ou senha inválidos'
+    //}
   }
-
 }
-
